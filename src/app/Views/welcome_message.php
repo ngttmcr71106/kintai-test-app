f2ba0c982d90c58c9ed74a0318a34f5a01f5679f
noguchi tomoki <noguchi.5tag@gmail.com>
2026-03-29 01:50:28 +0900

    srcソースコミット

---
+++ src/app/Views/welcome_message.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kintai app test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link rel="stylesheet" href="/css/output.css">
+</head>
+
+<body>
+    <div class="border p-4">
+        <p class="text-base">TailwindCSS</p>
+        <h1 class="font-bold text-4xl">unlock kintai</h1>
+        <p class="text-red-400 text-lg pt-6">test page</p>
+    </div>
+</body>
+
+</html>
